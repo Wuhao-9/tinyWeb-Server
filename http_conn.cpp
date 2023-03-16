4ee7a337c76f4c39b11dba9acb9362c440fb7afe
wuhao-Ubuntu <1017174960@qq.com>
2023-03-16 05:36:00 -0700

    通过有限状态机，实现了解析http请求报文的逻辑

--- http_conn.cpp
+++ http_conn.cpp
@@ -2,6 +2,7 @@
 #include "config.h"
 #include <cerrno>
 #include <iostream>
+#include <cstring>
 
 std::size_t http_conn::user_count = 0;
 
@@ -46,8 +47,11 @@ void http_conn::init(const int fd, const sockaddr_in& addr) {
 }
 
 void http_conn::process() {
-    std::cout << "I`s client request:" << std::endl;
-    std::cout << rd_buff_.buff_ << std::endl;
+    std::cout << rd_buff_.buff_;
+    auto cur_http_code = prase_recvData();
+    if (cur_http_code == NO_REQUEST) { // 若当前http状态码为NO_REQUEST，则请求不完整
+        // 解析
+    }
     std::string response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 11\r\n\r\nHello World";
     auto transfered_bytes = ::send(fd_, response.c_str(), response.size(), 0);
     if (transfered_bytes == -1) {
@@ -56,8 +60,160 @@ void http_conn::process() {
     }
 }
 
+http_conn::HTTP_CODE http_conn::prase_recvData() {
+    LINE_STATUS line_status = LINE_OK;
+    HTTP_CODE result = NO_REQUEST;
+    char* text = nullptr;
+
+    while ((cur_check_ == CHECK_CONTENT && line_status == LINE_OK) || (line_status = parse_one_line()) == LINE_OK) {
+        text = get_line();
+        new_line_idx_ = rd_buff_.check_idx_; // 更新至新一行的起始位置
+        std::clog << "[new line]: " << text << std::endl;
+        switch (cur_check_) {
+        case CHECK_REQUESTLINE:
+        {
+            result = parse_request_line(text);
+            if (result == BAD_REQUEST) 
+                return BAD_REQUEST;
+            break;
+        }
+        case CHECK_HEADER:
+        {
+            result = parse_request_header(text);
+            if (result == GET_REQUEST)
+                return handle_request();
+            break;
+        }
+        case CHECK_CONTENT:
+        {
+            result = parse_request_content(text); // 若没有获取到完整content，则返回NO_REQUEST
+            if (result == GET_REQUEST) 
+                return handle_request();
+            line_status = LINE_OPEN;
+            break;
+        }
+        default:
+            return INTERNAL_ERROR;
+        }
+    }
+    return NO_REQUEST;
+}
+
+http_conn::HTTP_CODE http_conn::handle_request() {
+    return HTTP_CODE::GET_REQUEST;
+}
+
+// 获取当前行首指针
+char* http_conn::get_line() {
+    return rd_buff_.buff_ + new_line_idx_;
+}
+
+// 从状态机，用于分析出一行内容
+// 返回值为行的读取状态，有LINE_OK, LINE_BAD, LINE_OPEN
+http_conn::LINE_STATUS http_conn::parse_one_line() {
+    char tmp = 0;
+    for (; rd_buff_.check_idx_ < rd_buff_.read_idx_; rd_buff_.check_idx_++) {
+        tmp = rd_buff_.buff_[rd_buff_.check_idx_];
+
+        if (tmp == '\r') {
+            if ((rd_buff_.check_idx_ + 1) == rd_buff_.read_idx_)
+                return LINE_OPEN;
+            else if (rd_buff_.buff_[rd_buff_.check_idx_+1] == '\n') {
+                rd_buff_.buff_[rd_buff_.check_idx_++] = '\0';
+                rd_buff_.buff_[rd_buff_.check_idx_++] = '\0';
+                return LINE_OK;
+            }
+            return LINE_BAD;
+        } else if (tmp == '\n') { // 第一次请求不完整(正好缺'\n'), 第二次分析请求行时则会进入当前if分支
+            if (rd_buff_.check_idx_ > 1 && rd_buff_.buff_[rd_buff_.check_idx_-1] == '\r') {
+                rd_buff_.buff_[rd_buff_.check_idx_-1] = '\0';
+                rd_buff_.buff_[rd_buff_.check_idx_++] = '\0';
+                return LINE_OK;
+            }
+            return LINE_BAD;
+        }
+    }
+    return LINE_OPEN; // 如果没找到\r\n则表示当前行不完整
+}
+
+// 解析http请求行，获得请求方法，目标url及http版本号
+http_conn::HTTP_CODE http_conn::parse_request_line(const std::string& text) {
+    auto pos = text.find_first_of(' ');
+    if (pos == std::string::npos) {
+        return BAD_REQUEST;
+    } else {
+        auto method = text.substr(0, pos);
+        if (method == "GET") {
+            requ_info_.method_ = GET;
+        } else if (method == "POST") {
+            requ_info_.method_ = POST;
+        } else { return BAD_REQUEST; }
+    }
+
+    std::string url = text.substr(pos+1);
+    pos = url.find_first_of(' ');
+    if (pos == std::string::npos) {
+        return BAD_REQUEST;
+    }
+    requ_info_.url_ = url.substr(0, pos);
+    if (requ_info_.url_.compare(0, 7, "http://") == 0) {
+        requ_info_.url_ = requ_info_.url_.substr(requ_info_.url_.substr(8).find_first_of('/'));
+    } else if (requ_info_.url_.compare(0, 8, "https://") == 0) {
+        requ_info_.url_ = requ_info_.url_.substr(requ_info_.url_.substr(8).find_first_of('/'));
+    }
+
+    if (requ_info_.url_.size() == 0 || requ_info_.url_[0] != '/')
+        return BAD_REQUEST;
+
+    std::string version = url.substr(pos + 1);
+    if (version != "HTTP/1.1") {
+        std::cerr << "[Unknow version]: " << version << std::endl;
+        return BAD_REQUEST;
+    } else {
+        requ_info_.version_ = std::move(version);
+    }
+
+    if (requ_info_.url_.size() == 1) { // 当url为'/'时，显示judge页面
+        requ_info_.url_ += "judge.html";
+    }
+    cur_check_ = CHECK_HEADER; // 改变主状态机状态
+    return NO_REQUEST;
+}
+
+http_conn::HTTP_CODE http_conn::parse_request_header(const std::string& text) {
+    if (text[0] == '\0') { // 当前行为request-header后紧跟的\r\n
+        if (requ_info_.content_len_ != 0) { // 如果有请求体，则继续读取
+            cur_check_ = CHECK_CONTENT; // 将主状态机状态更新为检查请求体
+            return NO_REQUEST;
+        }
+        return GET_REQUEST; // 若没有请求体，则直接解析请求、组装响应报文
+    } else if (text.compare(0, std::strlen("Connection:"), "Connection:") == 0) {
+        text.compare(12, std::string::npos, "keep-alive") == 0 ? requ_info_.linger_ = true : requ_info_.linger_ = false; 
+    } else if (text.compare(0, 15, "Content-length:") == 0) {
+        auto size = std::stoull(text.substr(16));
+        requ_info_.content_len_ = size;
+    } else if (text.compare(0, 5, "Host:") == 0) {
+        requ_info_.host_ = text.substr(6);
+    } else {
+        std::clog << "<Unknow header info>: " << text << std::endl;
+    }
+    return NO_REQUEST;
+}
+
+http_conn::HTTP_CODE http_conn::parse_request_content(const std::string& text) {
+    if (rd_buff_.read_idx_ >= rd_buff_.check_idx_ + requ_info_.content_len_) { // 检查buffer中的请求体内容是否完整
+        requ_info_.content_ = text.substr(0, requ_info_.content_len_);
+        return GET_REQUEST;
+    }
+    return NO_REQUEST;
+}
+
+// 新客户连接，重置"实现域"相关成员
 void http_conn::init() {
     status_ = -1;
     this->rd_buff_ = {};
     this->wr_buff_ = {};
+    this->requ_info_ = {};
+    cur_check_ = CHECK_REQUESTLINE;
+    new_line_idx_ = 0;
 }
--- http_conn.h
+++ http_conn.h
@@ -3,12 +3,48 @@
 
 #include <arpa/inet.h>
 #include <cstdlib>
+#include <string>
 
 class http_conn {
 public:
     const static std::size_t RD_BUFFER_SIZE = 1024 * 3;
     const static std::size_t WR_BUFFER_SIZE = 1024 * 3;
     static std::size_t user_count; 
+
+    enum METHOD {
+        GET = 1,
+        POST,
+        HEAD,
+        PUT,
+        DELETE,
+        TRACE,
+        OPTIONS,
+        CONNECT,
+        PATH
+    };
+
+    enum CHECK_STATE { // 有限状态机当前状态
+        CHECK_REQUESTLINE,
+        CHECK_HEADER,
+        CHECK_CONTENT
+    };
+
+    enum HTTP_CODE { // 当前http-request的状态
+        NO_REQUEST, // 请求不完整
+        GET_REQUEST,
+        BAD_REQUEST,
+        NO_RESOURCE,
+        FORBIDDEN_REQUEST,
+        FILE_REQUEST,
+        INTERNAL_ERROR,
+        CLOSED_CONNECTION
+    };
+
+    enum LINE_STATUS {
+        LINE_OK = 0,
+        LINE_BAD,   // 当前行信息格式错误
+        LINE_OPEN   // 当前行不完整
+    };
 
 public:
     http_conn() = default;
@@ -18,6 +54,14 @@ public:
     void setStatus(const char s) { if (s == 0 || s == 1) status_ = s; }
     void process();
 
+private:
+    http_conn::HTTP_CODE prase_recvData();
+    inline char* get_line();
+    http_conn::HTTP_CODE handle_request();
+    http_conn::LINE_STATUS parse_one_line();
+    HTTP_CODE parse_request_line(const std::string& text);
+    HTTP_CODE parse_request_header(const std::string& text);
+    HTTP_CODE parse_request_content(const std::string& text);
 private:
     int fd_;
     char status_; // read: 0, write: 1
@@ -33,6 +77,19 @@ private:
         char buff_[RD_BUFFER_SIZE];
     } wr_buff_;
 
+    // 解析http-request相关
+    CHECK_STATE cur_check_; // 主状态机解析进度
+    int new_line_idx_; // 新一行的开始下标
+    // request-info相关
+    struct {
+        std::string url_;
+        METHOD method_;
+        std::string version_;
+        std::string host_;
+        std::string content_;
+        std::size_t content_len_;
+        bool linger_;
+    } requ_info_;
 };
 
 #endif // HTTP_CONN_H_
