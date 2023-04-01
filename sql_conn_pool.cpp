9d22ebd0c48e4b64b424d0af3241e2940577a91e
wuhao-Ubuntu <1017174960@qq.com>
2023-04-01 10:54:31 -0700

    DB-pool is implemented

---
+++ sql_conn_pool.cpp
@@ -0,0 +1,76 @@
+#include "sql_conn_pool.h"
+#include <iostream>
+
+sql_conn_pool* sql_conn_pool::get_SQLpool_Instance() {
+    static sql_conn_pool connPool;
+    return &connPool;
+}
+
+void sql_conn_pool::init_connPool(std::string host, std::string user, std::string pwd, 
+            std::string dbName, uint32_t port, std::size_t maxConn, bool close_log) {
+    if (max_conn_num_ == 0) {
+        std::cerr << "Error: init zero SQL connection!" << std::endl;
+    }
+
+    this->host = host;
+    this->userName = user;
+    this->passWord = pwd;
+    this->DBName = dbName;
+    this->max_conn_num_ = maxConn;
+    this->close_log = close_log; 
+    this->Port = port;
+
+    for (int i = 0; i < max_conn_num_; i++) {
+        MYSQL* conn = ::mysql_init(nullptr); // init链接环境
+        if (conn == nullptr) {
+            std::cerr << "MYSQL init error" << std::endl;
+            exit(EXIT_FAILURE);
+        }
+        conn = ::mysql_real_connect(conn, host.c_str(), userName.c_str(), passWord.c_str(), DBName.c_str(), Port, nullptr, 0);
+        if (conn == nullptr) {
+            std::cerr << "MYSQL connect to target DB error" << std::endl;
+            exit(EXIT_FAILURE);
+        }
+        conn_list_.push_back(conn);
+        free_conn_num_++;
+    }
+}
+
+
+MYSQL* sql_conn_pool::getDBConn() {
+    MYSQL* conn = nullptr;
+    std::lock_guard<std::mutex> lock(mutex_);
+
+    if (conn_list_.size() == 0) {
+        return nullptr;
+    }
+
+    conn = conn_list_.front();
+    conn_list_.pop_front();
+    free_conn_num_--;
+    busy_conn_num_++;
+    return conn;
+}
+
+bool sql_conn_pool::releaseConn(MYSQL* conn) {
+    if (conn == nullptr)
+        return false;
+    
+    std::lock_guard<std::mutex> lock(mutex_);
+    conn_list_.push_back(conn);
+    free_conn_num_++;
+    busy_conn_num_--;
+    return true;
+}
+
+void sql_conn_pool::destroyPool() {
+    std::lock_guard<std::mutex> lock(mutex_);
+    if (conn_list_.size() > 0) {
+        for (MYSQL* conn : conn_list_) {
+            mysql_close(conn);
+        }
+        busy_conn_num_ = 0;
+        free_conn_num_ = 0;
+        conn_list_.clear();
+    }
+}
---
+++ sql_conn_pool.h
@@ -0,0 +1,49 @@
+#if !defined(SQL_CONN_POOL_H_)
+#define SQL_CONN_POOL_H_
+
+#include <mysql/mysql.h>
+#include <condition_variable>
+#include <string>
+#include <mutex>
+#include <list>
+
+class sql_conn_pool {
+public:
+	static sql_conn_pool* get_SQLpool_Instance(); // singleton pattern
+
+public:
+    void init_connPool(std::string host, std::string user, std::string pwd, std::string dbName, uint32_t port, std::size_t maxConn, bool close_log);
+    MYSQL* getDBConn();
+    bool releaseConn(MYSQL* conn);
+    int get_freeConn_num() const { return free_conn_num_; }
+    void destroyPool();
+    
+public:
+	std::string host;	        // 主机地址
+	std::string userName;		// 登陆数据库用户名
+	std::string passWord;	    // 登陆数据库密码
+	std::string DBName;         // 使用数据库名
+	uint32_t Port;		    // 数据库端口号
+	bool close_log;	            // 日志开关
+
+private:
+    sql_conn_pool() {
+        busy_conn_num_ = 0;
+        free_conn_num_ = 0;
+    }
+    
+    ~sql_conn_pool() {
+        destroyPool();
+    } 
+
+private:
+    std::size_t max_conn_num_;
+    std::size_t busy_conn_num_;
+    std::size_t free_conn_num_;
+    std::list<MYSQL*> conn_list_;
+    std::condition_variable cv_;
+    std::mutex mutex_;
+
+};
+
+#endif // SQL_CONN_POOL_H_
